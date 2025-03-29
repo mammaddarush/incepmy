ac5f23db05bfee10afbfa883ef10299a09aa0910
mammaddarush <43287529+mammaddarush@users.noreply.github.com>
2025-03-29 00:20:41 +0000

    hhdg

--- srcs/mariadb/mrun.sh
+++ srcs/mariadb/mrun.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
-
 # =============================================
 # MariaDB/MySQL Database Initialization Script
-
 # Database directory path
-DB_DIR="/var/lib/mysql"
-
+DataB_DIR="/var/lib/mysql"
+MYSQL_CONF_FILE="/etc/mysql/mariadb.conf.d/50-server.cnf"
+OLD_BIND_ADDRESS="127.0.0.1"
+NEW_BIND_ADDRESS="0.0.0.0"
 # --------------------------------------------------
 # Database Directory Initialization
-
-if [ ! -d "$DB_DIR/mysql" ]; then
-    echo "Initializing MariaDB data directory..."
-    mariadb-install-db --user=mysql --basedir=/usr --datadir="$DB_DIR"
+if [ ! -d "$DataB_DIR/mysql" ]; then
+    echo "start Building MD..."
+    mariadb-install-db --user=mysql --basedir=/usr --datadir="$DataB_DIR"
 fi
-
 # --------------------------------------------------
 # Network Configuration
-
-sed -i "s|bind-address\s*=\s*127.0.0.1|bind-address = 0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
+sed -i "s|bind-address\s*=\s*${OLD_BIND_ADDRESS}|bind-address = ${NEW_BIND_ADDRESS}|g" "$MYSQL_CONF_FILE"
 # --------------------------------------------------
 # Database Server Startup
-
 echo "Starting MariaDB..."
-mysqld_safe --datadir="$DB_DIR" &
+mysqld_safe --datadir="$DataB_DIR" &
 
 # --------------------------------------------------
 # Database Availability Check
-
 wait_for_mysql() {
     until mysqladmin ping --silent; do
         echo "Waiting for MySQL to be up..."
@@ -35,29 +29,20 @@ wait_for_mysql() {
     done
 }
 wait_for_mysql
-
-echo "Configuring MariaDB..."
-
 # --------------------------------------------------
 # Database Configuration
 
 mysql -u root <<-EOF
 
-    CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;
-    
-
+    CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;   
     CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'%';
-    
-
+    GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'%';    
     CREATE USER IF NOT EXISTS '${WORDPRESS_USER_ADMIN}'@'%' IDENTIFIED BY '${WORDPRESS_PASSWORD_ADMIN}';
     GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${WORDPRESS_USER_ADMIN}'@'%';
     GRANT ALL PRIVILEGES ON *.* TO '${WORDPRESS_USER_ADMIN}'@'%' WITH GRANT OPTION;
     
-
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${DATABASE_ROOT_PASSWORD}';
     
-
     FLUSH PRIVILEGES;
 EOF
 
